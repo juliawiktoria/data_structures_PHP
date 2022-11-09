2e3a4b25e00ec5ed021fd3ec59ffc3d59197c9e8
juliawiktoria <julia.w.zieba@gmail.com>
2022-11-09 15:24:38 +0000

    stack works but no limit conditions

--- stack.php
+++ stack.php
@@ -12,20 +12,23 @@ class Stack {
     function __construct($limit = 10, $empty_array = array()) {
       $this->stack = $empty_array;
       $this->limit = $limit;
+      $this->size = count($this->stack);
     }
 
     // other methods
 
     function print_stack() {
-      echo "The stack with limit $this->limit:\nBottom -> " . implode("--", $this->stack) . "\n";
+      echo "The stack of size $this->size with limit $this->limit:\nBottom -> " . implode("--", $this->stack) . "\n";
     }
 
-    function push($name) {
-      $this->name = $name;
+    function push($new_value) {
+      array_push($this->stack, $new_value);
+      $this->size++;
     }
 
     function pop() {
-      return $this->name;
+      $this->size--;
+      return array_pop($this->stack);
     }
 
     function get_size() {
@@ -38,5 +41,10 @@ class Stack {
   }
 
 $my_stack = new Stack(10, [1,2,3,4]);
+$my_stack->print_stack();
+$my_stack->push(55);
+$my_stack->print_stack();
+$popped = $my_stack->pop();
+echo "Popped element is $popped\n";
 $my_stack->print_stack();
 ?>
