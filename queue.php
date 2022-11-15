32b6c5e168492dbf20a8e90f54fdfe38ed91703d
juliawiktoria <julia.w.zieba@gmail.com>
2022-11-15 12:00:46 +0000

    queue finished

--- queue.php
+++ queue.php
@@ -3,20 +3,29 @@
 class Queue {
     function __construct($capacity=10) {
         $this->queue = [];
-        $this->front = 0;
-        $this->rear = 0;
+        $this->front = $this->rear = 0;
         $this->size = 0;
         $this->capacity = $capacity;
     }
 
     function enqueue($new_elem) {
-        $this->size++;
+        if(!$this->isFull()) {
+            array_push($this->queue, $new_elem);
+            $this->size++;
+            $this->rear = $this->size - 1;
+        } else {
+            echo "The queue is full, dequeue before enqueueing!\n";
+        }
     }
 
     function dequeue() {
         if(!$this->isEmpty()) {
-            $popped = 0;
+            // popping the first element of the queue off
+            $popped = array_shift($this->queue);
             $this->size--;
+
+            // updating the rear index
+            $this->rear = $this->size - 1;
             return $popped;
         } else {
             echo "Cannot dequeue of an empty queue\n";
@@ -24,17 +33,17 @@ class Queue {
         }
     }
 
-    function get_front() {
+    function getFront() {
         if(!$this->isEmpty()) {
-
+            return $this->queue[$this->front];
         } else {
             echo "The queue is empty, cannot return the front element!\n";
         }
     }
 
-    function get_rear() {
+    function getRear() {
         if(!$this->isEmpty()) {
-
+            return $this->queue[$this->rear];
         } else {
             echo "The queue is empty, cannot return the rear element!\n";
         }
@@ -42,7 +51,9 @@ class Queue {
 
     function display() {
         if(!$this->isEmpty()) {
-
+            echo "Front => " . implode("--", $this->queue) . " <= Rear\n";
+        } else {
+            echo "Cannot display an empty queue!\n";
         }
     }
 
@@ -53,4 +64,25 @@ class Queue {
     function getSize() {
         return count($this->queue);
     }
+
+    function isFull() {
+        return $this->getSize() == $this->capacity;
+    }
 }
+
+$my_queue = new Queue();
+
+$my_queue->display();
+$my_queue->enqueue(99);
+
+echo $my_queue->getFront();
+echo $my_queue->getRear();
+
+$my_queue->enqueue(11);
+$my_queue->enqueue(33);
+
+$my_queue->display();
+$deq = $my_queue->dequeue();
+
+echo $deq . "\n";
+$my_queue->display();
